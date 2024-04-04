fc3845be1ad51f63d5080d06d32593b7620219ba
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-04 10:30:04 +0900

    [commit]

---
+++ 01_lecture-source/chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section01/xmlconfig/Application.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section01.xmlconfig;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        /* 필기.
+        *   IoC(제어의 역전) 컨테이너
+        *   BeanFactory 란 스프링에서 관리하는 모든 객체를 Bean 이라고 한다.
+        *   스프링 컨테이너의 최상위 컨테이너이며, ApplicationContext 와 함께
+        *   스프링 컨데이너 라고 한다.
+        *   Bean의 생성과 설정, 관리, 조립 등의 역할을 맡고 있다.
+        *  */
+
+    }
+
+}
