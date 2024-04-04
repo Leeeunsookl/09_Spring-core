2e3dac1e5aa49bc50827667a17b5958bea5897b5
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-04 13:09:23 +0900

    [commit - 점심시간]

--- 01_lecture-source/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
+++ 01_lecture-source/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/Application.java
@@ -1,15 +1,17 @@
-package com.ohgiraffers.section01.xmlconfig;
+package com.ohgiraffers.section02.javaconfig;
 
 import com.ohgiraffers.common.MemberDTO;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = 
-                new GenericXmlApplicationContext("section01/xmlconfig/spring-context.xml");
+        // 자바 설정파일
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(ContextConfiguration.class);
 
         MemberDTO member = context.getBean(MemberDTO.class);
 
@@ -23,5 +25,5 @@ public class Application {
 
 
     }
-    
+
 }
