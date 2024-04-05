53534dbf5798fa935cdbc885520afaa878d32e1c
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-04-05 14:40:12 +0900

    [commit]

--- 01_lecture-source/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/common/Raccoon.java
+++ 01_lecture-source/chap03-di-annotation-lecture-source/src/main/java/com/ohgiraffers/section02/common/Raccoon.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+//@Primary
 public class Raccoon implements Animal{
     @Override
     public void eat() {
